77e57ad201622ec5cc16642dc0e259822bace3a6
Christoph Seitz <christoph.seitz@infineon.com>
2024-05-13 22:52:57 +0200

    Add aurix kernel reset helper.

--- soc/infineon/infineon_aurix/tc3xx/soc.h
+++ soc/infineon/infineon_aurix/tc3xx/soc.h
@@ -23,4 +23,12 @@ static inline int aurix_disable_clock(uintptr_t reg, uint32_t timeout)
 	return WAIT_FOR((sys_read32(reg) & 0x2) == 0x2, timeout, k_busy_wait(1));
 }
 
+static inline int aurix_kernel_reset(uint32_t krst0, uint32_t timeout)
+{
+	sys_write32(1, krst0 + 8);
+	sys_write32(1, krst0);
+	sys_write32(1, krst0 + 4);
+	return WAIT_FOR((sys_read32(krst0) & 0x2) == 0x2, timeout, k_busy_wait(1));
+}
+
 #endif
